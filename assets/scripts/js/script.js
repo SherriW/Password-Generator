62bb9867be3c4a6f22c9f087c6ee8da2b41d79d9
Sherri Winslow <sherriwinslow@Sherri-MBP-Business.local>
2021-08-15 20:50:51 -0700

    outlined rest of steps

--- assets/scripts/js/script.js
+++ assets/scripts/js/script.js
@@ -1,4 +1,11 @@
-// Get references to the #generate element
+// Steps for password generator project
+// 1. handle button click, you need to create a click event using the addEventListener for this generate button
+// 2. display - window.prompt() to ask the user for size of the password
+// 3. validate - only numbers and size of password, the range  >8 and <128
+// 4. prompt- window.confirm()  to ask user for ok or cancel for the type of password
+// 5. You need 4 confirms, each confirm represents the type of character in the password
+
+// Get references to the #generate element in HTML
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
